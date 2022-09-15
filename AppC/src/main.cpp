b06a7d89b51501d0af14f3e6540c6db539203ace
danj <danj@beamr.com>
2022-09-15 08:49:49 +0300

    added appc, fixed corer build and install

---
+++ AppC/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "core_c.h"
+
+using namespace std;
+
+int main() {
+  cout << "Hello world!" << endl;
+  cout << "Adding: " << add(4, 5) << endl;
+  return 0;
+}
+
--- CoreC/include/core_c.h
+++ CoreC/include/core_c.h
@@ -1,6 +1,14 @@
 #ifndef CORE_C_H
 #define CORE_C_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int add(int a, int b);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
