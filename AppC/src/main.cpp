eb80fa7ca700239aa64ba61f8df5ead0e3908132
danj <danj@beamr.com>
2022-09-15 10:36:57 +0300

    added some additional link dependencies for rust

--- AppC/src/main.cpp
+++ AppC/src/main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
+
 #include "core_c.h"
+#include "core_r.h"
 
 using namespace std;
+
+
 
 int main() {
   cout << "Hello world!" << endl;
   cout << "Adding: " << add(4, 5) << endl;
+  cout << "Product: " << product(4, 5) << endl;
   return 0;
 }
 
---
+++ CoreR/ffi/core_r.h
@@ -0,0 +1,14 @@
+#ifndef FFI_CORE_R_H
+#define FFI_CORE_R_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern int product(int a, int b);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //FFI_CORE_R_H
